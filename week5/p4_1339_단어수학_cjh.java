07c4173cd9c08822bab09d04a7fe3a847e09522e
ChoiJiHooon <159138577+ChoiJiHooon@users.noreply.github.com>
2024-07-24 14:01:49 +0900

    Add files via upload

---
+++ week5/p4_1339_단어수학_cjh.java
@@ -0,0 +1,45 @@
+package week5;
+
+import java.io.*; 
+import java.util.*; 
+
+public class p4_1339_단어수학_cjh {
+	static String[] words; 
+	static int[] weight; 
+	static int[] alpa; 
+	static int N; 
+	static int ans = 0; 
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		words = new String[N]; 
+		for(int i = 0; i < N; i++) {
+			 words[i] = br.readLine();
+		}
+		alpa = new int[26];
+		weight = new int[26];
+		decision(); 
+		System.out.println(ans);
+	}
+	static void decision() {   // 가중치를 더해주는 함수 
+		for(String word : words) {
+			int T = 1; 
+			for(int i= word.length() - 1; i >= 0; i-- ) {
+				weight[word.charAt(i) - 'A'] += T; 
+				T *= 10; 
+			}
+		}
+		Arrays.sort(weight); 
+		plus(); 
+	}
+	
+	static void plus() {       // 가중치를 구했으니 가중치가 가장 높은 순서대로 높은 숫자와 곱해서 더해줌 
+			int T = 9; 
+			for(int i = 25; i > 15; i-- ) {
+				ans = ans + ( weight[i]* T); 
+				T -=1; 
+			}
+		
+	}
+
+}
